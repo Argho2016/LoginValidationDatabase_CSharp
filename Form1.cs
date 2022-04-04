358296fbf9b3cae6988f9837aa4f1d50dfceb454
Argho Das <aiub2016@yahoo.com>
2022-04-04 12:36:50 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace loginDBZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-RLEUU0O;Initial Catalog=DBZ;User ID=sa;Password=123456");
+
+            string query = "Select * from member where id='" + textBox1.Text.Trim() + "' and password = '" + textBox2.Text.Trim() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlCon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+
+            if (dtbl.Rows.Count == 1)
+            {
+                Form2 f1 = new Form2();
+                this.Hide();
+                f1.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Wrong password");
+            }
+        }
+    }
+}
